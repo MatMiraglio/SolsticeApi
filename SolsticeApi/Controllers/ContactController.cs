b9beb7d4bc85aa5b9e082ec95fea17d60bf53791
goliath191 <miragliomatias@gmail.com>
2018-06-05 21:16:13 -0300

    Add action methods for CRUD operations

--- SolsticeApi/Controllers/ContactController.cs
+++ SolsticeApi/Controllers/ContactController.cs
@@ -6,12 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 using SolsticeApi.Models;
 using SolsticeApi.Repositories;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace SolsticeApi.Controllers
 {
     [Route("api/[controller]")]
-    public class ContactController : Controller
+    public class ContactController : ControllerBase
     {
         private IContactRepository repository;
 
@@ -23,34 +21,80 @@ namespace SolsticeApi.Controllers
 
         // GET: api/<controller>
         [HttpGet]
-        public IQueryable<Contact> Get()
+        public IQueryable<Contact> GetAllContacts()
         {
             return repository.GetContacts.OrderBy(p => p.ID);
         }
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IQueryable<Contact> GetContactById(int id)
         {
-            return "value";
+            return repository.GetContacts.Where(p => p.ID == id);
         }
 
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Post([FromBody]Contact newContact)
         {
+            if (newContact == null)
+            {
+                return BadRequest();
+            }
+
+            int newContactID = await repository.AddContact(newContact);
+            return CreatedAtRoute("GetContactById", new { Controller = "Contacts", id = newContactID }, newContact);
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> Put(int id, [FromBody]Contact contact)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contact.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public  IActionResult DeleteContact(int id)
         {
+            var contact = repository.GetContacts.Single(p => p.ID == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteContact(contact);
+
+            return Ok(contact);
         }
     }
 }
