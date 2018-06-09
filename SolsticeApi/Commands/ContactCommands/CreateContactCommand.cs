d75543e1f993fd88333d2d32c177895631fe8d7a
goliath191 <miragliomatias@gmail.com>
2018-06-09 19:22:59 -0300

    Add comments

--- SolsticeApi/Commands/ContactCommands/CreateContactCommand.cs
+++ SolsticeApi/Commands/ContactCommands/CreateContactCommand.cs
@@ -30,7 +30,9 @@ namespace SolsticeApi.Commands.ContactCommands
                 return new BadRequestResult();
             }
 
+            // The AddContact method returns the ID of the newly added contact as an int
             int newContactID = await contactRepository.AddContact(newContact);
+
             return new CreatedAtActionResult("GetContactById", "Contact", new { id = newContactID }, newContact);
         }
 
