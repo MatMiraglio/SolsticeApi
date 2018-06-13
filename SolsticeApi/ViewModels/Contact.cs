3f96e3c68171d9f45f1333e420002d1627552701
goliath191 <miragliomatias@gmail.com>
2018-06-13 09:36:50 -0300

    Add ViewModels

---
+++ SolsticeApi/ViewModels/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolsticeApi.ViewModels
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string ProfilePicFileName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string PhoneNumberWork { get; set; }
+        public string PhoneNumberHome { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ SolsticeApi/ViewModels/SaveContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SolsticeApi.ViewModels
+{
+    public class SaveContact
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string PhoneNumberWork { get; set; }
+        public string PhoneNumberHome { get; set; }
+        public string Address { get; set; }
+    }
+}
