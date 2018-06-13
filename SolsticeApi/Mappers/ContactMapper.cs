30b5d8a4107d3b885fbe0aca4ad8e6fda88a6d0c
goliath191 <miragliomatias@gmail.com>
2018-06-13 09:34:50 -0300

    Add Mappers

---
+++ SolsticeApi/Mappers/ContactMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolsticeApi.Mappers
+{
+    public class ContactMapper : Profile
+    {
+        public ContactMapper()
+        {
+            CreateMap<ViewModels.SaveContact, Models.Contact>();
+            CreateMap<Models.Contact, ViewModels.Contact>();
+        }
+    }
+}
